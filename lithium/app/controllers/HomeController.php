b5386264e5b3714efe6985436c43932111ffd420
jed <sdf>
2012-04-06 20:47:18 -0700

    .

--- lithium/app/controllers/HomeController.php
+++ lithium/app/controllers/HomeController.php
@@ -20,6 +20,42 @@ class HomeController extends \lithium\action\Controller
     // Oh come on! Add a random function mongo.
     public function fetch()
     {
+    	$stories=array();
+    	$conditions = array('status' => 'accepted' );
+    	$count= Story::count(compact('conditions'));
+    	for($loop=0; $loop<100; $loop++)
+    	{   	
+    		//echo("$loop ");
+    		$page = mt_rand( 0,$count);
+    		
+    		
+    		$story = Story::find('first',compact('conditions','page'));
+    		if($story)
+    		{
+    			//echo($story['_id']);
+    			$dup=false;
+    			foreach($stories as $value)
+    			{
+    				if($value['_id']==$story['_id']) 
+    				{
+    					$dup=true;
+    					break;
+    				}
+    			}
+    			if(!$dup)
+    			{
+    				//array_push($stories,$story);
+    				$stories[] = $story;
+    				if(count($stories)>=9) break;
+    			}
+    		}
+    		
+    	}
+    	$stories = new RecordSet(array('data' => $stories));
+    	
+		$this->render(array('json' => compact('stories')));
+    	
+    	
     	/*
     	$limit=6;
     	$conditions = array('status' => 'accepted');
@@ -32,6 +68,8 @@ class HomeController extends \lithium\action\Controller
 		$this->render(array('json' => compact('stories')));
 		
 		*/
+		
+		/*
     	//$stories=array('stories' => {} );
     	$stories=array();
  
@@ -40,7 +78,7 @@ class HomeController extends \lithium\action\Controller
     		//echo("$loop ");
     		$rand = mt_rand( 1331937596,time());
     		//$rand = mt_rand( time()-100,time());
-    		
+    		echo("$rand ");
     		$conditions = array('status' => 'accepted', 'updated' => array('$gte' => $rand)  );
     		$story = Story::find('first',compact('conditions'));
     		if($story)
@@ -71,6 +109,7 @@ class HomeController extends \lithium\action\Controller
 		$this->render(array('json' => compact('stories')));
 		//$this->render(array('json' => array('stories' => $stories)));
 		//$this->render(array('json' => $stories));
+		*/
 		
     }
     
