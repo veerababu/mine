c1141b33c756e073aafd5b7f9ac3c56d2fefca8f
misoon <misoon>
2012-04-12 23:00:33 -0700

    .

--- lithium/app/views/pages/contact.html.php
+++ lithium/app/views/pages/contact.html.php
@@ -1,5 +1,5 @@
 <div class="row">
 	<?php  echo $this->_render('element', 'cta_nav'); ?>
 	<div class="span9">
-		Email us! <h1> info@bravoYourCity.com </h1>
+		Email us! <h1> info@bravoyourcity.com </h1>
 </div></div>
