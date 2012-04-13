582c4effa708ea634efdde474b3d0e277d788cf3
misoon <misoon>
2012-04-12 22:37:23 -0700

    .

--- lithium/app/views/pages/faq.html.php
+++ lithium/app/views/pages/faq.html.php
@@ -5,10 +5,12 @@
 <h1>GENERAL</h1> 
 
 <h3>What is Bravo Your City?</h3>
-BRAVO YOUR CITY is a world travel and social enrichment webzine with written and visual content that is entirely user-generated but is edited like a professional magazine. BYC publishes authentic, unique stories for world travelers written by locally-based or well-traveled contributors of the destination city, making it uniquely different than what is currently out there. Not only will popular destinations be featured but under-the-radar locations will be added as the BYC community grows. The diverse content is created by a large group of world contributors, which is then curated by professional editors. BYC's signature submission parameters are: one location, accompanied by 1-5 photos and 1-500 words of text. These simple guidelines make it easy to submit multiple stories.
+Bravo Your City is a global travel and social enrichment webzine with written and visual content that is entirely user generated but is edited and published like a professional magazine. BYC publishes authentic and unique stories for the world -- by the world -- about the world. The diverse content is created by a large group of global contributors, which is then curated by professional editors. BYC's signature submission parameters of: 1 location, 1-5 photos and 1-500 words of text make it easy to submit multiple stories.
 
 <h3>What is your purpose?</h3>
-The aim of BYC is to serve local communities while providing a resource for global travelers. It's a platform for global travelers who are looking for a more authentic traveling experience, but it is also for locals who want to learn and share about what's happening in their community while giving back to it at the same time through BYC 50% profit sharing. 
+The aim of BYC is to create a global platform for people to contribute a variety of travel, human interest and social enrichment stories in a professional way. We think there are enough people in the world who have a love for writing and photography -- who may not be able to work full time as a writer or photographer -- but will enjoy seeing what happens with their creative freelancing efforts on BYC. 
+
+		<p>Our aim is to serve local communities while providing a resource for people to learn about their global neighbors. It's a place for global travelers who are looking for a more authentic traveling experience, but it is also for locals who want to learn and share about what's happening in their community while giving back to it at the same time through BYC <a href="/pages/50">50% profit sharing</a>.
 
 <h3>Why is it called Bravo Your City?</h3>
 It's a little wink to <a href="http://bravoyourlife.com">Bravo Your Life!</a> -- the travel essay/memoir book the founder MiSoon Burzlaff wrote before she started BYC. 
@@ -18,7 +20,10 @@ It's a little wink to <a href="http://bravoyourlife.com">Bravo Your Life!</a> --
 
 
 <h3>How can I become a contributing writer or photographer?</h3>
-Just click <a href="/story/edit">submit</a> 
+Just click <a href="/story/edit">share</a>. Remember our submission guidelines for each story are: 1 location, 1-5 photos and 1-500 words.  
+
+<h3>Do you own my stories I submit? May I use them on other sites or my blog?</h3> 
+When you submit a story to BYC, you license it to us, but you still retain your copyright and can use it for your blog or other things. 
 
 <h3>May I submit just photos or just writing?</h3> 
 Yes. We think the submission guidelines keep it simple and sweet, so try your hand at a little photography or writing a few sentences or a paragraph even if you think writing is not your forte. You can also pair up with a friend and submit photos and writing as a duo. Just give credit to the photographer in the captions and have the writer submit the story. In the Beta release, there will be a section for writers to submit stories in search of photographers and vice versa. We think this might be a fun way to collaborate and meet someone new in your community. 
@@ -29,8 +34,15 @@ No. We do encourage you to submit the most about what you know. It makes sense t
 <h3>What kind of cities do you want content for?</h3> 
 We want places you think need to be on the BYC map. Of course, we want all the glossy cosmopolitan cities, but we also want BYC to be a resource for what's going on in small towns (like in Napa Valley or upstate New York) or mid-sized cities you might not think people are interested in. Travelers do find themselves in Cleveland, Little Rock, Minneapolis, Sacramento, Narita, Incheon, Lyon, Cornwall, etc and they will want to use BYC to learn about these off-the-radar locations.
 
+<h3>What are tags?</h3> 
+Tags help us lead readers to your stories or your profile. It's another way of saying categories that apply to your story.
+
+<h3>How should I tag my story?</h3> 
+When you submit the story, add as many tags as you would like that help us categorize your story. For example, if you are writing about a Japanese restaurant in the Mission that specializes in ramen and is a very cozy spot, perfect to bring a date, you would tag your story: food, Japanese, ramen, noodles, restaurant, dates, romantic. The location is tagged when you add the address and neighborhood. As the site grows, it will autosuggest the most common tags as you begin to type them, but feel free to add your own tags if there is no suggestion.
+
 <h3>Do you pay contributors for content that gets published to BYC?</h3> 
-Yes!
+Yes! If your story is curated into a BYC Microguide and bought by other users, you will be paid. Since we are just starting out, we need more stories before Microguides can be in full effect. <a href="/story/edit">Please share your story!</a>  
+
 <!--
 <h3>How much do you pay contibutors?</h3>
 Contributors will receive 5% of the profit from their stories that are sold.
