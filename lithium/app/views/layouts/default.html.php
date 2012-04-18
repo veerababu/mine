20f3c9b76c0e406351aebc450fef8f21d0aae0d2
jed <sdf>
2012-04-18 09:11:20 -0700

    .

--- lithium/app/views/layouts/default.html.php
+++ lithium/app/views/layouts/default.html.php
@@ -15,9 +15,9 @@
     <script src="/js/jquery-ui.js"></script>
     <script src="/js/bootstrap.js"></script>
     
-	<link href="/css/bravo.css?13" rel="stylesheet">
+	<link href="/css/bravo.css?14" rel="stylesheet">
     <script src="/js/date.format.js?7"></script>
-    <script src="/js/bravo.js?14"></script>
+    <script src="/js/bravo.js?15"></script>
     
 	
 	
