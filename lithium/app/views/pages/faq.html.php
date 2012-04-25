99774d41225c2d8e445408675cbf0f33b1468781
misoon <misoon>
2012-04-25 11:47:05 -0700

    .

--- lithium/app/views/pages/faq.html.php
+++ lithium/app/views/pages/faq.html.php
@@ -36,7 +36,7 @@ BYC wants to publish any story that you think is interesting. We like traditiona
 We want places you think need to be on the BYC map. Of course, we want all the glossy cosmopolitan cities, but we also want BYC to be a resource for what's going on in small towns (like in Napa Valley or upstate New York) or mid-sized cities you might not think people are interested in. Travelers do find themselves in Cleveland, Little Rock, Minneapolis, Sacramento, Narita, Incheon, Lyon, Cornwall, etc and they will want to use BYC to learn about these under-the-radar locations.
 
 <h3>What size photos should I upload?</h3> 
-For now, medium sized JPG that are medium to large in quality are the best. Anything over 1.5 MB will take too long to load. 
+For now, medium sized JPG that are medium/ high in quality are the best. Anything over 1.5 MB will take too long to load. 
 
 <h3>What are tags?</h3> 
 Tags help us lead readers to your stories or your profile. It's another way of saying categories that apply to your story.
